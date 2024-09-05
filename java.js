c44a3790f39d893f5892e7a2c30f34cebf09aaca
freneticmango <79719031+freneticmango@users.noreply.github.com>
2024-09-05 12:08:27 -0700

    Add keyboard support

--- java.js
+++ java.js
@@ -1,5 +1,7 @@
-//Logic for updating the display with number clicked
+//Updates the display with number clicked, limits the number of decimal places displayed.
 const display = document.querySelector("#display");
+
+let displayValue = "";
 
 function limitDisplay(value) {
     return +parseFloat(value).toFixed(14);
@@ -10,8 +12,33 @@ function updateDisplay (element) {
     display.textContent = limitDisplay(displayValue);
 }
 
-let displayValue = "";
+//Clears the display and all numerical values
+const clearKey = document.querySelector("#clear");
 
+function clearDisplay() {
+    display.textContent = "";
+    displayValue = "";
+    firstOperand = "";
+    secondOperand = ""; 
+}
+
+clearKey.addEventListener("click", () => clearDisplay());
+
+
+//Clears one digit from the end of the number displayed
+const backspaceKey = document.querySelector("#backspace");
+
+function backspace() {
+    display.textContent = display.textContent.slice(0, -1);
+    displayValue = display.textContent;
+}
+
+backspaceKey.addEventListener("click", () => {
+    backspace();
+});
+
+
+//Adds click event listeners for each number key
 const numberKeys = document.querySelectorAll(".number");
 
 numberKeys.forEach((numKey) => {
@@ -26,19 +53,7 @@ numberKeys.forEach((numKey) => {
     };
 });
 
-
-//Logic for clearing display 
-const clearKey = document.querySelector("#clear");
-
-const clearDisplay = () => {
-    display.textContent = ""
-    displayValue = "";
-    firstOperand = "";
-    secondOperand = ""; 
-}
-clearKey.addEventListener("click", () => clearDisplay());
-
-//Object containing functions to perform operations
+//Object containing functions to perform operations, accessible with strings
 let math = {
     add: function (firstNum, secondNum) {
         return (+firstNum) + (+secondNum);
@@ -58,53 +73,119 @@ let math = {
 }
 
 
-//Variables for holding operands and operatorKeys for calculation
+//Variables for holding operands and operators for calculation
 let firstOperand;
 let secondOperand;
+
 let selectedOperator;
 
 let operatorPressed;
 
-//Each operator gets the first operand and the operation to be performed, and then clears the display for the next operand. operatorPressed is set to true.
+//Each operator gets the first operand and the operation to be performed. The stored value is cleared, and operatorPressed is set to true.
 const operatorKeys = document.querySelectorAll(".operator");
 
-operatorKeys.forEach((operator) => {
-    operator.addEventListener("click", () => {
-        firstOperand = displayValue;
-        displayValue = "";
-        selectedOperator = operator.id;
-        operatorPressed = true;
-    });
+function selectOperator(opKey) {
+    firstOperand = displayValue;
+    displayValue = "";
+    selectedOperator = opKey.id;
+    operatorPressed = true;
+}
+
+operatorKeys.forEach((opKey) => {
+    opKey.addEventListener("click", () => selectOperator(opKey));
 });
 
 //The equalsKey button gets the second operand and performs an operation if operatorPressed is true. Once the operation is performed, operatorPressed is set to false.
 const equalsKey = document.querySelector("#equals");
 
-equalsKey.addEventListener("click", () => {
-    secondOperand = displayValue;
-
-    if (operatorPressed) {
-        let result = math[selectedOperator](firstOperand,secondOperand);
-        firstOperand = displayValue = result;
-        display.textContent = limitDisplay(displayValue); 
-        operatorPressed = false;
+function performOperation() {
+    if (operatorPressed && displayValue) {
+        secondOperand = displayValue;
+        if (secondOperand == 0 && selectedOperator == 'divide') {
+            display.textContent = "No dividing by zero!";
+        } else {
+            let result = math[selectedOperator](firstOperand,secondOperand);
+            firstOperand = displayValue = result;
+            display.textContent = limitDisplay(displayValue); 
+            operatorPressed = false;
+        };
     };
+}
+
+equalsKey.addEventListener("click", () => {
+    performOperation();
 });
 
+
+//Changes the sign of the displayed value
 const changeSignKey = document.querySelector("#changeSign");
 
-changeSignKey.addEventListener("click", () => {
+function changeSign() {
     displayValue *= (-1);
     display.textContent = displayValue;
+}
+
+changeSignKey.addEventListener("click", () => {
+    changeSign();
 });
 
 
+//Converts displayed value to a percentage
 const percentKey = document.querySelector("#percent");
 
-percentKey.addEventListener("click", () => {
+function makePercent() {
     displayValue /= 100;
+}
+
+percentKey.addEventListener("click", () => {
+    makePercent();
 });
 
 
+//Add keyboard support for each button
+document.addEventListener('keydown', handleKeys);
 
+function handleKeys(event) {
+    numberKeys.forEach((numKey) => {
+        if (event.key == numKey.textContent)
+            updateDisplay(numKey);
+    })
 
+    operatorKeys.forEach((opKey) => {
+        if (event.key == opKey.textContent)
+            selectOperator(opKey)
+    })
+
+    switch (event.key) {
+        case "=":
+        case "Enter": 
+            performOperation();
+            break;
+        case "%":
+            makePercent();
+            break;
+        case "c":
+            changeSign();
+            break;
+        case "Backspace":
+            backspace();
+            break;
+        case "Delete":
+            clearDisplay();
+    }
+
+    // if (event.key == "=" || event.key == "Enter")
+    //     performOperation();
+
+    // if (event.key == "%")
+    //     makePercent();
+
+    // if (event.key == "c")
+    //     changeSign();
+
+    // if (event.key == "Backspace")
+    //     backspace();
+
+    // if (event.key == "Delete")
+    //     clearDisplay();
+}
